4122d2cfd83e5a4d3bb72821333cef59857ecaef
Marina Filippova <alira.noki@gmail.com>
2025-03-24 23:48:30 +0300

    Добавила тест на поиск, который не выдает результатов

--- search_selene_DuckDuckGo_test.py
+++
@@ -1,9 +0,0 @@
-from selene import browser, have
-from conftest import setup_browser
-
-def test_search_element(setup_browser):
-    browser.open("https://duckduckgo.com/")
-    browser.element('[id=searchbox_input]').type('qa.guru').press_enter()
-    browser.element('[id=web_content_wrapper]').should(have.text('https://qa.guru'))
-
-
---
+++ search_selene_duck_duck_go_test.py
@@ -0,0 +1,16 @@
+from selene import browser, have
+from conftest import setup_browser
+
+def test_search_element(setup_browser):
+    browser.open("https://duckduckgo.com/")
+    browser.element('[id=searchbox_input]').type('qa.guru').press_enter()
+    browser.element('[id=web_content_wrapper]').should(have.text('https://qa.guru'))
+
+def test_result_not_found(setup_browser):
+    browser.open("https://duckduckgo.com/")
+    searchedText = 'иромро'
+    browser.element('[id=searchbox_input]').type(searchedText).press_enter()
+    actualResult = 'По запросу '+searchedText+' результаты не найдены.'
+    browser.element('//section[@data-testid="mainline"]//span').should(have.text(actualResult))
+
+
